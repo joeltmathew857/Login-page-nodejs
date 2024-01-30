295465df0e6bfaef1ca6b8e819d510c42541af33
joel thomas mathew <joelmathew857@gmail.com>
2024-01-30 15:18:07 +0530

    add ima

--- app.js
+++ app.js
@@ -1,58 +1,47 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const session = require("express-session");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 
-app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: "your-secret-key",
+    secret: "this is the secret key",
     resave: false,
-    saveUninitialized: true,
-    cookie: { maxAge: 60000 }, // Set the session timeout in milliseconds
+    saveUninitialized: false,
   })
 );
 
+//listening request
 app.listen(3000);
 
-app.get("/", (req, res) => {
-  res.render("login", { message: req.session.message });
+app.get("/login", (req, res) => {
+  if (req.session.isAuth) {
+    res.redirect("/");
+  }
+  res.render("login");
 });
 
-// ... (previous code)
+const data = { user: "joel", pass: "1234" };
 
 app.post("/login", (req, res) => {
-    const username = req.body.name;
-    const password = req.body.password;
-  
-    // Replace the following condition with your predefined username and password
-    const predefinedUsername = "joel mathew";
-    const predefinedPassword = "poormanwork";
-  
-    if (username === predefinedUsername && password === predefinedPassword) {
-      req.session.user = username;
-      req.session.message = null;
-      res.redirect("/home");
-    } else {
-      req.session.message = "Incorrect username or password";
-      res.redirect("/");
-    }
-  });
-  
-  // ... (remaining code)
-  
+  if (data.user === req.body.name && data.pass === req.body.password) {
+    res.render("index");
+    console.log(req.body);
+  }
+  res.redirect("login");
+});
 
-app.get("/home", (req, res) => {
-  if (req.session.user) {
-    res.render("home");
+app.get("/", (req, res) => {
+  if (req.session.isAuth) {
+    res.render("index");
   } else {
-    res.redirect("/");
+    res.redirect("/login");
   }
 });
 
-app.get("/logout", (req, res) => {
-  req.session.destroy();
-  res.redirect("/");
-});
+app.get("/logout", (req, res) => {});
