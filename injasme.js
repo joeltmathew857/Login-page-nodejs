449ba584ea36e0fa9e3e93cd7be0b740cc2c4408
joel thomas mathew <joelmathew857@gmail.com>
2024-02-01 12:37:34 +0530

    add

--- injasme.js
+++ injasme.js
@@ -1,1 +1,18 @@
 const fs = require("fs");
+const path = require("path");
+// fs.readFile("./text.js", "utf-8", (err, data) => {
+//   fs.unlink();
+//   // if(err){
+//   //     console.log(err,'error message 1')
+//   // }
+//   console.log(data);
+// });
+
+// fs.writeFile("./text.js", "ijhgff", () => {});
+
+// // fs.mkdir("./hellohu.js", () => {
+// //   console.log("folder");
+// // });
+console.log(path);
+// delete fs.mkdir("./hello",()=>{});
+fs.mkdir("./johns/samplesa/fideriom", () => {});
