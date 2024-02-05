8bb3e622b95590ea0d7afdb9e59bc2750d6db942
joel thomas mathew <joelmathew857@gmail.com>
2024-02-05 17:45:31 +0530

    add change

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const bodyParser = require("body-parser");
 const express = require("express");
 const session = require("express-session");
+const nocache = require("nocache");
+const path = require("path");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
+app.use(nocache());
+
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.listen(3000);
 
 app.use(
   session({
@@ -15,8 +22,6 @@ app.use(
     saveUninitialized: false,
   })
 );
-
-app.listen(3000);
 
 app.get("/login", (req, res) => {
   if (req.session.isAuth) {
@@ -29,13 +34,13 @@ const data = { user: "joel", pass: "1234" };
 
 app.post("/login", (req, res) => {
   if (data.user === req.body.name && data.pass === req.body.password) {
+    req.session.isAuth = true;
     res.render("index");
     console.log(req.body);
+  } else {
+    res.redirect("/login");
   }
-  res.redirect("login");
 });
-
-
 
 app.get("/", (req, res) => {
   if (req.session.isAuth) {
@@ -45,4 +50,11 @@ app.get("/", (req, res) => {
   }
 });
 
-app.get("/logout", (req, res) => {});
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.ejs"));
+});
+
+app.get("/logout", (req, res) => {
+  delete req.session.isAuth;
+  res.redirect("/login");
+});
